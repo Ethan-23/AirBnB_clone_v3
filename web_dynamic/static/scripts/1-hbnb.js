6992b59ee3ccb3931303f21e227f3c9f78674f77
Ethan-23 <emanroberts@gmail.com>
2021-05-17 03:42:13 +0000

    added fully functioning task 1

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,11 @@
-if(document.readyState === "complete") {
-
-}
+$(document).ready(function () {
+  const listof = {};
+  $('INPUT').change(function () {
+    if ($(this).prop('checked')) {
+      listof[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete listof[$(this).attr('data-id')];
+    }
+    $('DIV.amenities h4').text(Object.values(listof).join(', '));
+  });
+});
